a9dfb0c4e6920f96ca20163c5f8bd656e9e88924
numerikdavlok <david.pavlik@unil.ch>
2022-01-25 17:50:54 +0100

    png to svg; debut js (gsap)

---
+++ main/main.js
@@ -0,0 +1,21 @@
+// Page 1
+
+// Page 2
+
+// Page 3
+
+// Page 4
+
+// Page 5
+// Mouvements texte sur parasol
+gsap.to(".para1", {rotation: 27, x: 100, duration: 1});
+
+// Page 6
+
+// Page 7
+
+// Page 8
+
+// Page 9
+
+// Page 10
